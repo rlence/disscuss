1cdee979b0e23718179ecaf4b871b76ee6f822dd
rlence-d <ricardo@doofinder.com>
2022-04-27 15:21:44 +0200

    coonect to postgres

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ import Config
 
 # Configure your database
 config :discuss, Discuss.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "rlence",
+  password: "rlencepassword",
   hostname: "localhost",
-  database: "discuss_dev",
+  database: "postgres",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
