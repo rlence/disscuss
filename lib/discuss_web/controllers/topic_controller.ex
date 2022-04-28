ed48b7fa74c6b18e66a1f1c052ba24197ef118a5
rlence-d <ricardo@doofinder.com>
2022-04-28 19:20:07 +0200

    redirect

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -4,6 +4,11 @@ defmodule DiscussWeb.TopicController do
   alias DiscussWeb.Topic
   alias Discuss.Repo
 
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render conn, "new.html", changeset: changeset
@@ -13,7 +18,10 @@ defmodule DiscussWeb.TopicController do
     changeset = Topic.changeset(%Topic{}, title)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect(post)
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: Routes.topic_path(conn, :index))
       {:error, changeset} ->
           render conn, "new.html", changeset: changeset
     end
